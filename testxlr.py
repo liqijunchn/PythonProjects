ee7e02c850a0dc4e306885e2332a3143db5e3ae0
liqijunchn <liqijunchn@163.com>
2018-03-14 11:09:30 +0800

    modified

--- testxlr.py
+++ testxlr.py
@@ -93,3 +93,11 @@ ws['B1'] = '3.14%'
 wb.guess_types = False
 ws['B1'].value
 ws['B1'].number_format
+
+
+import datetime
+from openpyxl import Workbook
+wb = Workbook()
+ws = wb.active
+# set date using a Python datetime
+ws['A1'] = datetime.datetime(2010, 7, 21)
